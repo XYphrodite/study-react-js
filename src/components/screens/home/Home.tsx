47d8b00931266b468a110a4c3eca4f4fb3d5074d
Maksim <mrmaksoracle89537089833@yandex.ru>
2023-11-15 15:19:36 +0300

    added useMemo()

--- src/components/screens/home/Home.tsx
+++ src/components/screens/home/Home.tsx
@@ -1,15 +1,21 @@
 
 
+import { useMemo } from 'react';
 import CarItem from './car-item/CarItem'
 const res = await fetch('http://localhost:3000/cars');
 const cars = await res.json();
+
 const Home = () => {
+    const filteredCars = useMemo(
+        () => cars.filter(car => car.price>=1000), []
+        );
+    
     return (
         <>
         <div>
             <h1>My catalog</h1>
             <div>
-                {cars.length ? cars.map(car => (
+                {filteredCars.length ? filteredCars.map(car => (
                     <CarItem key={car.id} car={car}/>
                 )) 
                 : <p>There are no cars</p>}
